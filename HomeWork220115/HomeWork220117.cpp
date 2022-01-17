eebfcc0f59cc041d67e2c466ca93c89e26611bf4
태권 김 <minggy69@gmail.com>
2022-01-18 02:41:31 +0900

    김태권 : 220117 숙제 못함, 난 바보야

---
+++ HomeWork220115/HomeWork220117.cpp
@@ -0,0 +1,36 @@
+﻿// HomeWork220115.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int StringCount(const char* _Text)
+{
+    int Count = 0;
+    while (0 != _Text[Count]);
+    {
+        Count += 1;
+    }
+    return Count;
+}
+
+
+int CountFirst(const char* _Text, int _Start, const char* _FindStr)
+{
+    int Count = 0;
+    if 
+    
+    for (int i = 0; i > 4; i += 1);
+    {
+
+    }
+}
+
+
+int main()
+{
+    
+    {
+        int Count = CountFirst("aaa eee ttt asdfasd eee", 15, "eee");
+    }
+  
+}
