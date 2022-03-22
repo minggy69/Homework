ac99b7106c33e3465852cf12a608d2fca74d8f0e
태권 김 <ktk0240@gmail.com>
2022-03-23 02:45:51 +0900

    김태권: 220323 idle 이미지 경로 수정

--- API/GameEngineContents/Puyopuyo2.cpp
+++ API/GameEngineContents/Puyopuyo2.cpp
@@ -19,8 +19,8 @@ void Puyopuyo2::GameInit()
 
 	// 리소스를 다 로드하지 못하는 상황이 올수가 없다.
 
-	GameEngineImageManager::GetInst()->Load("D:\\Project\\AR40\\API\\Resources\\Image\\Idle.bmp", "Idle.bmp");
-	GameEngineImageManager::GetInst()->Load("D:\\Project\\AR40\\API\\Resources\\Image\\HPBAR.Bmp", "HPBAR.Bmp");
+	GameEngineImageManager::GetInst()->Load("C:\\Homework\\API\\Resources\\Image\\Idle.bmp", "Idle.bmp");
+	GameEngineImageManager::GetInst()->Load("C:\\Homework\\API\\Resources\\Image\\HPBAR.Bmp", "HPBAR.Bmp");
 
 
 	CreateLevel<TitleLevel>("Title");
