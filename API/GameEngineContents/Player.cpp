d2fda1c9780d9657a94abcb64fd39e683ae3891e
태권 김 <ktk0240@gmail.com>
2022-03-25 02:28:16 +0900

    김태권: 220324 복습

--- API/GameEngineContents/Player.cpp
+++ API/GameEngineContents/Player.cpp
@@ -2,7 +2,8 @@
 #include <GameEngine/GameEngine.h>
 #include <GameEngineBase/GameEngineWindow.h>
 #include <GameEngine/GameEngineImageManager.h>
-
+#include <GameEngineBase/GameEngineInput.h>
+#include <GameEngineBase/GameEngineTime.h>
 Player::Player()
 {
 }
@@ -19,6 +20,48 @@ void Player::Start()
 
 	CreateRenderer("Idle.bmp");
 	CreateRendererToScale("hpbar.bmp", float4(300.0f, 20.0f), RenderPivot::CENTER, float4(0.0f, -100.0f));
+
+	if (false == GameEngineInput::GetInst()->IsKey("MoveLeft"))
+	{
+		// 이때 대문자여야 합니다.
+		GameEngineInput::GetInst()->CreateKey("MoveLeft", 'A');
+		GameEngineInput::GetInst()->CreateKey("MoveRight", 'D');
+		GameEngineInput::GetInst()->CreateKey("MoveUp", 'W');
+		GameEngineInput::GetInst()->CreateKey("MoveDown", 'S');
+		GameEngineInput::GetInst()->CreateKey("Jump", VK_LSHIFT);
+		GameEngineInput::GetInst()->CreateKey("Fire", VK_SPACE);
+		// VK_LBUTTON;
+	}
+}
+
+void Player::Update()
+{
+	if (true == GameEngineInput::GetInst()->IsPress("MoveLeft"))
+	{
+		SetMove(float4::LEFT /** GameEngineTime::GetDeltaTime()*/);
+	}
+
+	if (true == GameEngineInput::GetInst()->IsPress("MoveRight"))
+	{
+		SetMove(float4::RIGHT);
+	}
+
+	if (true == GameEngineInput::GetInst()->IsPress("MoveUp"))
+	{
+		SetMove(float4::UP);
+	}
+
+	if (true == GameEngineInput::GetInst()->IsPress("MoveDown"))
+	{
+		SetMove(float4::DOWN);
+	}
+
+	// 내가 키를 눌렀다면 움직여라.
+	//if (0 != GetAsyncKeyState('A'))
+	//{
+	//	int a = 0;
+	//}
+
 }
 
 // 랜더러가 다 돌아가고 랜더링이 됩니다.
